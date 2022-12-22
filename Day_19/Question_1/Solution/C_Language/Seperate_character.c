45b0f7b3b1df599b0172e2f0f61ebf6a0b0431cb
Haze45 <adityaansu142@gmail.com>
2022-12-22 23:55:02 +0530

    Day_19
    Separate character in String

---
+++ Day_19/Question_1/Solution/C_Language/Seperate_character.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[256];
+    int i, len;
+    // int alpha = 0, digit = 0, specialChar = 0, i;
+    printf("C program to separate character \n");
+    printf("Please Enter a String :  ");
+    gets(str);
+    len = strlen(str);
+    printf("Printing Character of string\n");
+    for (i = 0; i < len; i++)
+    {
+        printf("%c \n", str[i]);
+    }
+    return 0;
+}
