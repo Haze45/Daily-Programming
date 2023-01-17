886b942be0583b40c06c24492171b451360ca33c
Haze45 <adityaansu142@gmail.com>
2023-01-17 19:26:00 +0530

    Day_27
    Sum and Average of the entered number until o is entered

---
+++ Day_27/Question_1/Solution/C_Language/Sum_and_Avg_until_0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   double x,sum = 0,avg = 0,i;
+    while (x != 0){
+  cout<<"Enter the number : ";
+  cin>>x;
+
+    sum = sum + x;
+
+    avg = sum / i;
+  i++;
+}
+cout<<"Sum of the numbers is "<<sum<<endl;
+cout<<"Average of the numbers is "<<avg;
+return 0;
+}
